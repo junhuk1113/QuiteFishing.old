be57a93ac7de5cc5d1130ca650778db01ce5388f
junhuk1113 <junhuk1113@naver.com>
2024-02-09 23:09:23 +0900

    Change fabric mapping official to yarn

---
+++ Common/src/main/java/net/pmkjun/quitefishing/mixin/FishingBobberSlientMixin.java
@@ -0,0 +1,9 @@
+package net.pmkjun.quitefishing.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+import net.minecraft.client.multiplayer.ClientPacketListener;
+import net.minecraft.network.PacketListener;
+
+@Mixin(ClientPacketListener.class)
+public class FishingBobberSlientMixin {
+}
